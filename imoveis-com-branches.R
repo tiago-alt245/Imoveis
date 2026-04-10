5698195f373c3be2161a1fc2704c1031252c4194
tiago-alt245 <htopimentel@id.uff.br>
2026-04-10 10:11:20 -0300

    Script com gráficos

--- imoveis-com-branches.R
+++ imoveis-com-branches.R
@@ -2,5 +2,6 @@
 dados = read.csv("imoveis - imoveis.csv", header=TRUE, sep=",")
 
 # 2. Gráficos
+hist(dados$metragem, main = "Histograma da Metragem", xlab = "Metragem", ylab = "Dados")
 
 # 3. Medidas
